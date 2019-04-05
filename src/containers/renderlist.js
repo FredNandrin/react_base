ebbbe6b83c55f4d7dc3f308bbf8a627fc80634f6
Frédéric Nandrin <fred@nanit.be>
2019-04-05 23:15:27 +0200

    Static auth with token (still need auth token retreiving)

--- src/containers/renderlist.js
+++ src/containers/renderlist.js
@@ -16,7 +16,14 @@ class RenderList extends Component {
         // Call the API page
 
         this.setState({ itemsAreLoading: true });
-        fetch('http://127.0.0.1:8000/api/test_entities.json')
+        const id_token='eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE1NTQ0OTg0NDQsImV4cCI6MTU1NDUwMjA0NCwicm9sZXMiOlsiUk9MRV9VU0VSIl0sInVzZXJuYW1lIjoiZnJlZEBuYW5pdC5iZSJ9.ddzaE53kf5cXQQXUkIC4DL-ro-0TQUZxf_DtbLRBq41LS-FqSMoUQ6juvsGT6UuB9jlkdyOQjULr-D0gdqZxdJmNJr-0WIv879UjZYR9_6yc9Cr6oYbG1-8DXNKZBmk13R4pKihQe5FeBi63QXQhSX_L5I_S0kXjbk3br9nRHvx_a1gLbLPyN27vU5Ebno6mEEluANIp7ySn0jFIm0_Tt-CAC1r_ur9Kkb-IFqmcwvPRki_MBf95gc3OTeM2EQxRwTS-lvXaP10bLrAd5eu5GE1iMoHtmObmNDaPBJX3jJLTvgu1J3TaJ2__RlMZLoy9-1dq1RoJ5iKkQ_I8kATiJpR_rMgfTzWVs0t7ODAx70vCVMu_kV2GNlCNLMNeWdfslW2OWf2mID9sBLEGtuHj00Nr-WySeGs1vPciaG5HgUHRWf871S-ZxLdXV2j6BbhV8CPXNIivHirpKPemz6lbW69KnjtXkfBBUbiy75sJAF7_eYVGT6OOuBHrCukIt7PJKWSOnv92zdVCXv_vYjWptAlyExGCy_k6yLPZ_lnNvLf3DGs8iAI6YMmiLtmcXqV_RdopBkReNdZrtdazEUUXVs6HIulykxNxsm703rphpLjEkM7wYeEHY1rD9KXju76ePHLP2J53uBJkkBee3sqOvMNHTpcHK33IjzSVLt-Ahdk';
+        const headers = {'AUTHORIZATION': 'Bearer '+id_token};
+        fetch('http://localhost:8000/api/test_entities.json', {
+            method: 'GET',
+            headers: headers
+        })
+
+
             .then((result) => {
                 // Get the result
                 // If we want text, call result.text()
@@ -24,7 +31,7 @@ class RenderList extends Component {
             }).then((jsonResult) => {
             // Do something with the result
             console.log(jsonResult);
-            this.setState( { items: jsonResult, itemsAreLoading: true  });
+            this.setState( { items: jsonResult, itemsAreLoading: false  });
         })
     }
 
@@ -46,7 +53,7 @@ class RenderList extends Component {
       {items.map(list_item =>
         <div key={list_item.id}>
           <Link to={{pathname:'/listitem/' + list_item.id, state:{list_item} }} style={{marginTop: '25px', padding: '5px'}}>
-            List Item {list_item.id}
+            List Item {list_item.id} : {list_item.name}
           </Link>
           <br />
           <br />
